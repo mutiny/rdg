18ab18f1f147976e7521b11248889bb9deeafbd8
Louis Rose <louismrose@gmail.com>
2015-03-17 17:03:16 +0000

    Moved conditional integration specs into their own folder.

--- spec/integration/cfg/conditionals/if_spec.rb
+++ spec/integration/cfg/conditionals/if_spec.rb
@@ -3,21 +3,13 @@ require "rdg/cfg"
 module RDG
   describe CFG do
     context "for simple if expressions" do
-      it "should show control flowing from predicate to successor (skipping consequence)" do
+      it "should show control flowing from predicate through and around consequence" do
         cfg = CFG.from_source("a = 1; if b == 1 then; c = 1; end; z = 1")
 
         expect(cfg).to contain("a = 1", "b == 1", "c = 1", "z = 1")
 
         expect(cfg).to flow_between("a = 1", "b == 1")
         expect(cfg).to flow_between("b == 1", "z = 1")
-      end
-
-      it "should show control flowing into predicate and through consequence" do
-        cfg = CFG.from_source("a = 1; if b == 1 then; c = 1; end; z = 1")
-
-        expect(cfg).to contain("a = 1", "b == 1", "c = 1", "z = 1")
-
-        expect(cfg).to flow_between("a = 1", "b == 1")
         expect(cfg).to flow_between("b == 1", "c = 1")
         expect(cfg).to flow_between("c = 1", "z = 1")
       end
@@ -32,6 +24,17 @@ module RDG
         expect(cfg).to flow_between("c = 1", "d = 1")
         expect(cfg).to flow_between("d = 1", "e = 1")
         expect(cfg).to flow_between("e = 1", "z = 1")
+      end
+
+      it "should show control flowing through and around predicate of modifier if" do
+        cfg = CFG.from_source("a = 1; c = 1 if b == 1; z = 1")
+
+        expect(cfg).to contain("a = 1", "b == 1", "c = 1", "z = 1")
+
+        expect(cfg).to flow_between("a = 1", "b == 1")
+        expect(cfg).to flow_between("b == 1", "z = 1")
+        expect(cfg).to flow_between("b == 1", "c = 1")
+        expect(cfg).to flow_between("c = 1", "z = 1")
       end
     end
 
@@ -63,6 +66,18 @@ module RDG
         expect(cfg).to flow_between("c = 1", "z = 1")
         expect(cfg).to flow_between("d = 1", "z = 1")
         expect(cfg).to flow_between("e = 1", "z = 1")
+      end
+
+      it "should show control flowing from predicate to both consequences of ternary" do
+        cfg = CFG.from_source("a = 1; b == 1 ? c = 1 : d = 1; z = 1")
+
+        expect(cfg).to contain("a = 1", "b == 1", "c = 1", "d = 1", "z = 1")
+
+        expect(cfg).to flow_between("a = 1", "b == 1")
+        expect(cfg).to flow_between("b == 1", "c = 1")
+        expect(cfg).to flow_between("b == 1", "d = 1")
+        expect(cfg).to flow_between("c = 1", "z = 1")
+        expect(cfg).to flow_between("d = 1", "z = 1")
       end
     end
   end
