9aff5c9315ea1182bf3fac7796a5f23bfaf69cda
Louis Rose <louismrose@gmail.com>
2015-04-20 17:29:24 +0100

    Add missing unit tests for CFG analysers.

---
+++ spec/unit/control/ensure_spec.rb
@@ -0,0 +1,33 @@
+require "rdg/control/ensure"
+
+module RDG
+  module Control
+    describe Ensure do
+      let(:registry) { spy('registry') }
+      let(:context) { Analysis::Context.new(spy('graph'), spy('equivalences'), registry) }
+
+      let(:main_ast) { FakeAst.new(:if) }
+      let(:ensure_ast) { FakeAst.new(:send) }
+      let(:ast) { FakeAst.new(:rescue, children: [main_ast, ensure_ast]) }
+      subject { Ensure.new(ast, context) }
+
+      it "should have control flow start at the main block" do
+        expect(subject.start_node).to eq(main_ast)
+      end
+
+      it "should have control flow end at the ensure block" do
+        expect(subject.end_nodes).to eq([ensure_ast])
+      end
+
+      it "should have control flow from main block to ensure block" do
+        expect(subject.internal_flow_edges).to eq([[main_ast, ensure_ast]])
+      end
+
+      it "should prepend Handler so that edges back to main_ast are created for ensure block" do
+        subject.analyse
+
+        expect(registry).to have_received(:prepend_for).with(ensure_ast, Handler)
+      end
+    end
+  end
+end
--- spec/unit/control/rescue_spec.rb
+++ spec/unit/control/rescue_spec.rb
@@ -3,13 +3,16 @@ require "rdg/control/rescue"
 module RDG
   module Control
     describe Rescue do
+      let(:registry) { spy('registry') }
+      let(:context) { Analysis::Context.new(spy('graph'), spy('equivalences'), registry) }
+
       let(:main_ast) { FakeAst.new(:if) }
       let(:handler1) { FakeAst.new(:send) }
       let(:handler2) { FakeAst.new(:raise) }
 
       context "rescue without an else" do
         let(:ast) { FakeAst.new(:rescue, children: [main_ast, handler1, handler2, :""]) }
-        subject { Rescue.new(ast) }
+        subject { Rescue.new(ast, context) }
 
         it "should have control flow start at the main block" do
           expect(subject.start_node).to eq(main_ast)
@@ -22,11 +25,18 @@ module RDG
         it "should have no control flow edges (yet)" do
           expect(subject.internal_flow_edges).to eq([])
         end
+
+        it "should prepend Handler so that edges back to main_ast are created for each handler" do
+          subject.analyse
+
+          expect(registry).to have_received(:prepend_for).with(handler1, Handler)
+          expect(registry).to have_received(:prepend_for).with(handler2, Handler)
+        end
       end
 
       context "rescue with an else" do
         let(:ast) { FakeAst.new(:rescue, children: [main_ast, handler1, handler2, :else]) }
-        subject { Rescue.new(ast) }
+        subject { Rescue.new(ast, context) }
 
         it "should have control flow start at the main block" do
           expect(subject.start_node).to eq(main_ast)
@@ -38,6 +48,13 @@ module RDG
 
         it "should have control flow from the main block to the else and nothing more (yet)" do
           expect(subject.internal_flow_edges).to eq([[main_ast, :else]])
+        end
+
+        it "should prepend Handler so that edges back to main_ast are created for each handler" do
+          subject.analyse
+
+          expect(registry).to have_received(:prepend_for).with(handler1, Handler)
+          expect(registry).to have_received(:prepend_for).with(handler2, Handler)
         end
       end
     end
