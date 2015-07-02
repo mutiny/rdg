133535c0c1f22d095a787f4deabdd325f8eaac4b
Louis Rose <louismrose@gmail.com>
2015-07-02 12:18:19 +0100

    Avoid parallel assignment.

--- spec/support/doubles/fake_ast.rb
+++ spec/support/doubles/fake_ast.rb
@@ -2,7 +2,10 @@ class FakeAst
   attr_accessor :type, :children, :ancestors, :siblings
 
   def initialize(type, children: [], ancestors: [], siblings: [])
-    @type, @children, @ancestors, @siblings = type, children, ancestors, siblings
+    @type = type
+    @children = children
+    @ancestors = ancestors
+    @siblings = siblings
   end
 
   def empty?
